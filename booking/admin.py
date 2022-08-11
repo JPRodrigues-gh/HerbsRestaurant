14e141e43109009ee8aefcd31496709d7e759f89
JPRodrigues-gh <jp.rodrigues.ba@gmail.com>
2022-08-11 20:50:10 +0000

    Add TableAdmin and BookingAdmin classes and modify ContactAdmin class

--- booking/admin.py
+++ booking/admin.py
@@ -4,7 +4,20 @@ from django_summernote.admin import SummernoteModelAdmin
 from .models import Table, User, Booking, Contact
 
 
-@admin.register(Table, User, Booking, Contact)
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    """ Add filter functionality to Table table on open and table_size"""
+    list_filter = ('open', 'table_size',)
+
+
+@admin.register(User, Contact)
 class ContactAdmin(SummernoteModelAdmin):
     """ Add summernote field functionality to contact.body field"""
+    list_filter = ('name', 'surname', 'created_date',)
     summernote_fields = ('body')
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    """ Add filter functionality to Booking table on booking_date"""
+    list_filter = ('booking_date',)
