25141595703902ca71c95829b54c59cf51e20103
JPRodrigues-gh <jp.rodrigues.ba@gmail.com>
2022-08-20 16:12:10 +0000

    Add whitenoise settings

--- booking/urls.py
+++ booking/urls.py
@@ -10,4 +10,4 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
     path('booking/', views.BookingList.as_view(), name='booking'),
-]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
--- herbs/settings.py
+++ herbs/settings.py
@@ -111,6 +111,7 @@ else:
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
