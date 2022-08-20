6dd7ea6cbd5826ca32dbd7fa8cba732dd31a3a12
JPRodrigues-gh <jp.rodrigues.ba@gmail.com>
2022-08-20 15:31:23 +0000

    Add config for the statis file mime issue

--- booking/urls.py
+++ booking/urls.py
@@ -1,4 +1,6 @@
 ''' url.py file in the booking app folder '''
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
@@ -8,4 +10,4 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
     path('booking/', views.BookingList.as_view(), name='booking'),
-]
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
