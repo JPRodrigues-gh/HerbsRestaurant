d9e57da1544137ba25398cfc1d54c162e085a5f5
JPRodrigues-gh <jp.rodrigues.ba@gmail.com>
2022-08-29 12:30:23 +0000

    Reconfig cloudinary

--- herbs/settings.py
+++ herbs/settings.py
@@ -226,7 +226,7 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 #     'API_SECRET': os.environ["CLOUDINARY_API_SECRET"]
 # }
 
-CLOUDINARY_URL = CLOUDINARY_URL
+CLOUDINARY_URL = os.environ.get('CLOUDINARY_URL', False)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
