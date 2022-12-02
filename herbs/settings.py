7bbd6981f52a4050fa8ac93101aaa28ebcccc6e7
JPRodrigues-gh <jp.rodrigues.ba@gmail.com>
2022-12-02 17:59:29 +0000

    Comment out STATIC_ROOT, error creating super user

--- herbs/settings.py
+++ herbs/settings.py
@@ -210,7 +210,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = [os.path.join(BASE_DIR, 'staticfiles')]
+# STATIC_ROOT = [os.path.join(BASE_DIR, 'staticfiles')]
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
