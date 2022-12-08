71ec387802a31a936f440d1de47553fd04fd9a1e
JPRodrigues-gh <jp.rodrigues.ba@gmail.com>
2022-12-08 20:15:06 +0000

    Set DEBUG=True

--- herbs/settings.py
+++ herbs/settings.py
@@ -37,7 +37,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['herbsrestaurantv2.herokuapp.com',
                  'localhost']
 
