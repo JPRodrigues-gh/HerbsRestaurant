327abc2ce6964304b048bf59673480df0087c034
JPRodrigues-gh <jp.rodrigues.ba@gmail.com>
2022-12-09 09:10:22 +0000

    Remove False from CLOUDINARY_URL

--- herbs/settings.py
+++ herbs/settings.py
@@ -233,7 +233,10 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 #     'API_SECRET': os.environ["CLOUDINARY_API_SECRET"]
 # }
 
-CLOUDINARY_URL = os.environ.get('CLOUDINARY_URL', False)
+# CLOUDINARY_URL = os.environ.get('CLOUDINARY_URL', False)
+CLOUDINARY_STORAGE = {
+    'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
