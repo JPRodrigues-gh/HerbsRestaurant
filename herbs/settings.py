9dfed024ed2df2d1e2a80c2519cf30dae2e6b59d
JPRodrigues-gh <jp.rodrigues.ba@gmail.com>
2022-12-09 09:12:30 +0000

    Remove commented out CLOUDINARY_STORAGE settings

--- herbs/settings.py
+++ herbs/settings.py
@@ -227,13 +227,6 @@ STATICFILES_FINDERS = (
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-# CLOUDINARY_STORAGE = {
-#     'CLOUD_NAME': os.environ["CLOUDINARY_CLOUD_NAME"],
-#     'API_KEY': os.environ["CLOUDINARY_API_KEY"],
-#     'API_SECRET': os.environ["CLOUDINARY_API_SECRET"]
-# }
-
-# CLOUDINARY_URL = os.environ.get('CLOUDINARY_URL', False)
 CLOUDINARY_STORAGE = {
     'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
 }
